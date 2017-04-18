88002d3c57fe25a3ad6977126e21a6002de8a487
Shanicky Chen <shanickyc@gmail.com>
2017-04-18 22:40:50 +0800

    add some key binding

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -17,6 +17,21 @@
 (require 'init-local-rust)
 
 
+
+(global-set-key [f1] 'comment-or-uncomment-region)
+(global-set-key [f3] 'indent-buffer)
+(global-set-key [C-tab] 'other-window)
+(global-set-key (kbd "<C-f9>") 'open-eshell-other-buffer)
+(global-set-key (kbd "C-c ;") 'ska-point-to-register)
+(global-set-key (kbd "C-c '") 'ska-jump-to-register)
+;;(global-set-key [mouse-4] 'down-slightly)
+;;(global-set-key [mouse-5] 'up-slightly)
+(global-set-key (kbd "C-x k") 'kill-this-buffer)
+(global-set-key (kbd "C-x a r") 'align-regexp)
+(global-set-key (kbd "M-[") 'align)
+(global-set-key (kbd "C-c C-p") 'md/duplicate-down)
+(global-set-key (kbd "C-c C-P") 'md/duplicate-up)
+
 ;;(require 'go-impl)
 
                                         ;(add-hook 'c++-mode-hook 'irony-mode)
@@ -35,5 +50,8 @@
                                         ;    'irony-completion-at-point-async))
                                         ;(add-hook 'irony-mode-hook 'my-irony-mode-hook)
                                         ;(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+
+
 
 (provide 'init-local)
